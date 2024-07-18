b0657b6944ce5a146978ba7ef8e093f0a17dff24
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-07-18 19:59:30 +0200

    Create authenticationCheck middleware

--- routes/user.routes.mjs
+++ routes/user.routes.mjs
@@ -1,9 +1,20 @@
 import express from "express";
 const router = express.Router();
 
+const isAuthenticatedUserMiddleware = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect("/user/no-permission");
+  }
+  next();
+};
+
 router.get("/no-permission", (req, res) => {
   res.render("noPermission");
 });
+
+// Set up authentication middleware
+router.use(isAuthenticatedUserMiddleware);
+// Beyond this point, only authenticated users can access the routes
 
 router.get("/logged", (req, res) => {
   res.render("logged");
